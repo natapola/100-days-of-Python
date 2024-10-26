9700cd6478d98d2696ad747b27270f55bc1a4ec1
kosichkina <nataliia.poladova@gmail.com>
2024-10-26 21:05:27 +0300

    Input

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,4 @@
 print('Welcome to the band name generator')
-city = input(What is the name of the city you grew up in?n)
-pet = input(What is your pets name?n)
-print(Your band name could be  + city +   + pet)
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is your pets name?\n")
+print("Your band name could be " + city + " " + pet)
