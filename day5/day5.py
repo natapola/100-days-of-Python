6d1668305ada8caef38770c88400c9ecb9065a31
kosichkina <nataliia.poladova@gmail.com>
2024-10-28 21:22:50 +0200

    Password Generator

---
+++ day5/day5.py
@@ -0,0 +1,46 @@
+import random
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Solution 1
+password = ""
+for char in range(0, nr_letters): #(1, nr_letters + 1)
+    password += random.choice(letters)
+
+for sym in range(0, nr_symbols): #1, nr_symbols +1
+    password += random.choice(symbols)
+
+for num in range(0, nr_numbers): #1, nr_numbers +1
+    password += random.choice(numbers)
+
+print(password)
+
+
+# Solution 2
+
+password_list = []
+
+for char in range(nr_letters):
+    password_list.append(random.choice(letters))
+
+for sym in range(nr_symbols):
+    password_list.append(random.choice(symbols))
+
+for num in range(nr_numbers):
+    password_list.append(random.choice(numbers))
+
+# mix the list
+random.shuffle(password_list)
+
+# collect the mixed symbols in a line
+password = "".join(password_list)
+
+print(f"Your generated password is: {password}") 
