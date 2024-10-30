61264301984ce6cf7980727011f688c11e747397
kosichkina <nataliia.poladova@gmail.com>
2024-10-30 14:21:34 +0200

    Reeborg's World

---
+++ day6/day6.py
@@ -0,0 +1,21 @@
+# Solution de level Maze on Reeborg's World
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()    
+
+while not at_goal():
+    
+    if right_is_clear():
+        turn_right()
+        move()
+    
+    elif front_is_clear():
+        move()
+    else:
+        turn_left() 
